ad4ab127b6b17b63428fb320da90b7870e68af1d
Kodirov <kaddzumi@gmail.com>
2024-04-18 00:02:52 +0900

    Commit Character Creation and Animation Setup

---
+++ Source/DarkUnit/Private/Character/CharacterBase.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/CharacterBase.h"
+
+
+ACharacterBase::ACharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("RightWeaponSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
+}
+
+void ACharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
---
+++ Source/DarkUnit/Public/Character/Player/PlayerAnimInstance.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PlayerAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DARKUNIT_API UPlayerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	UPlayerAnimInstance();
+	UFUNCTION(BlueprintCallable)
+	void LocalNativeUpdateAnimation(float DeltaTime);
+	
+private:
+	//References
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="References", meta = (AllowPrivateAccess = "true"))
+	class APlayerCharacterBase* Player;
+	
+	// Movement
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Movement", meta = (AllowPrivateAccess = "true"))
+	float Speed;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Movement", meta = (AllowPrivateAccess = "true"))
+	float Direction;	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Movement", meta = (AllowPrivateAccess = "true"))
+	bool bCrouching;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Movement", meta = (AllowPrivateAccess = "true"))
+	bool bInAir;
+public:
+
+};
