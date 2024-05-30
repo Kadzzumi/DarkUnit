d2f6152a1ee6cee7aa7821b42378fac37a4b5b3a
Kodirov Iskander <iskanderkodirov29@gmail.com>
2024-05-30 13:29:37 +0900

    Create MMC_CurseResistance.cpp

---
+++ Source/DarkUnit/Private/AbilitySystem/ModMagCalc/MMC_CurseResistance.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/ModMagCalc/MMC_CurseResistance.h"
+#include "AbilitySystem/MainAttributeSet.h"
+#include "Interaction/CombatInterface.h"
+
+UMMC_CurseResistance::UMMC_CurseResistance()
+{
+	StrengthDef.AttributeToCapture = UMainAttributeSet::GetStrengthAttribute();
+	StrengthDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	StrengthDef.bSnapshot = false;
+
+	RelevantAttributesToCapture.Add(StrengthDef);
+}
+
+float UMMC_CurseResistance::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	// Gather tags from source and target
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluateParameters;
+	EvaluateParameters.SourceTags = SourceTags;
+	EvaluateParameters.TargetTags = TargetTags;
+	
+	float Strength = 0.f;
+	GetCapturedAttributeMagnitude(StrengthDef, Spec, EvaluateParameters, Strength);
+	Strength = FMath::Max<float>(Strength, 0);
+	
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
+	const int32 PlayerLevel = CombatInterface->GetPlayerLevel();
+
+	return	10 + Strength + PlayerLevel/2;
+}
