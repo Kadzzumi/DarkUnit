6488e03ad7e66d49621fcb6eef98c5652666e41c
Kodirov Iskander <iskanderkodirov29@gmail.com>
2024-05-30 13:22:16 +0900

    Create MMC_Evasion.h

---
+++ Source/DarkUnit/Public/AbilitySystem/ModMagCalc/MMC_Evasion.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayModMagnitudeCalculation.h"
+#include "MMC_Evasion.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DARKUNIT_API UMMC_Evasion : public UGameplayModMagnitudeCalculation
+{
+	GENERATED_BODY()
+public:
+	UMMC_Evasion();
+	virtual float CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const override;
+	
+private:
+	FGameplayEffectAttributeCaptureDefinition StrengthDef;
+};
