9cb6ea9ccbc1e2dcdb9f22484c46e5bf8628edfb
Kodirov Iskander <iskanderkodirov29@gmail.com>
2024-05-30 13:13:36 +0900

    Update MCC_Defense.h

--- Source/DarkUnit/Public/AbilitySystem/ModMagCalc/MCC_Defense.h
+++ Source/DarkUnit/Public/AbilitySystem/ModMagCalc/MCC_Defense.h
@@ -1,1 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
 
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayModMagnitudeCalculation.h"
+#include "MMC_Defense.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DARKUNIT_API UMMC_Defense : public UGameplayModMagnitudeCalculation
+{
+	GENERATED_BODY()
+public:
+	UMMC_Defense();
+	virtual float CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const override;
+	
+private:
+	FGameplayEffectAttributeCaptureDefinition StrengthDef;
+};
